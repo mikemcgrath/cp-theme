ce255c1921749afff02143204e15ceab6ecf0978
Michael McGrath <mmcgrath@mcgrathsolutions.org>
2014-12-12 15:16:16 -0500

    initial code

---
+++ block--navigation.tpl.php
@@ -0,0 +1,1 @@
+<?php echo TheCPGroup_Final_SS_art_hmenu_output($content);?>
---
+++ templates/comment-wrapper.tpl.php
@@ -0,0 +1,5 @@
+<?php
+	$vars = get_defined_vars();
+	$view = TheCPGroup_Final_SS_art_get_drupal_view();
+	$view->print_comment_wrapper($vars);
+?>
